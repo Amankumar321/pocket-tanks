8834b77b770989d8026242fab437aee3a2c4852b
Vandana kumari <vandanakumari@Vandanas-MacBook-Air.local>
2023-08-20 23:34:10 +0530

    20-8-23

--- client/src/socket/index.js
+++ client/src/socket/index.js
@@ -1,7 +1,6 @@
 import io from 'socket.io-client'
 
 var local = 'http://localhost:5001'
-var wifi = 'http://192.168.0.108:5001'
 var url = 'https://pocket-tanks.onrender.com'
 
-export const socket = io(wifi)
+export const socket = io(url)
